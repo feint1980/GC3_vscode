a1c9318c65ea0e182b9a8863c1b8bbea2425a5de
Feint <otaku2986@gmail.com>
2024-06-25 20:45:18 +0700

    ltg, adding AST

--- GC3/src/PaternBehavior_from_lua.h
+++ GC3/src/PaternBehavior_from_lua.h
@@ -1,6 +1,51 @@
 #pragma once 
 #include "PaternBehaviorBase.h"
 
+
+enum Operator{
+    NONE,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    SQRT,
+    COS, 
+    SIN
+};
+
+struct Node {
+    float value = 0;
+    Node * left;
+    Node * right;
+    Operator op = NONE;
+
+    float getValue() {
+        switch (op) {
+            case ADD:
+                return left->getValue() + right->getValue();
+            case SUB:
+                return left->getValue() - right->getValue();
+            case MUL: 
+                return left->getValue() * right->getValue();
+            case DIV:   
+                return left->getValue() * right->getValue();
+            case SQRT:
+                return sqrt(left->getValue());
+            case COS:
+                return cos(left->getValue());
+            case SIN:
+                return sin(left->getValue());
+            default:
+                return value;
+        }
+    }   
+
+
+
+
+};
+
+
 class PaternBehavior_from_lua : public PaternBehaviorBase
 {
 public:
@@ -9,7 +54,7 @@ public:
     virtual void update(float deltaTime)override;
 
 
-    void setXFactor(float x);
+    void setXFactor(float x);   
     void setYFactor(float y);
 
     void setRadius(float radius);
