3de44ae3493d4261d307c00a615828da18b6b65a
Feint <otaku2986@gmail.com>
2024-09-11 21:31:07 +0700

    adding db

---
+++ TB_Server/manual/sqlite3_command_note.sql
@@ -0,0 +1,13 @@
+-- Note for sqlite3 command
+--
+
+
+-- Schema for the table base 
+
+declare @account_table_name as varchar(128) = 'accounts_table'
+
+create table [IF NOT EXISTS] @account_table_name (
+    accound_no int not null auto_increment ,
+
+
+)
