8c97481caa68659e289afe9b625007da0fe95b59
Feint <otaku2986@gmail.com>
2024-09-23 19:51:54 +0700

    update table

--- TB_Server/data/sqlite3_command_note.sql
+++ TB_Server/data/sqlite3_command_note.sql
@@ -6,8 +6,58 @@ drop table if exists account_table;
 
 create table account_table (
     account_no INTEGER primary key AUTOINCREMENT,
-    account_name varchar(128) not null, -- account name, primary key
+    account_id varchar(128) not null, -- account name, primary key
     account_password varchar(128) not null -- account password
 
 );
 
+create table account_stats (
+    account_stat_id varchar(128) primary key, -- primary key
+    mm_rate INTEGER, 
+    gold DOUBLE, 
+    account_id varchar(128) not null,
+    CONSTRAINT fk_account FOREIGN KEY (account_id)
+    REFERENCES account_table(account_id)
+
+);
+
+create table character_table (
+    character_id varchar(128) primary key, -- primary key
+    character_name varchar(128) not null,
+    character_last_name varchar(128) not null,
+    character_titla varchar(128),
+    character_str INTEGER,
+    character_vit INTEGER,
+    character_dex INTEGER,
+    character_agi INTEGER,
+    character_int INTEGER,
+    character_wis INTEGER
+);
+
+create table account_own_character (
+    count_id INTEGER primary key AUTOINCREMENT,
+    additional_str INTEGER,
+    additional_vit INTEGER,
+    additional_dex INTEGER,
+    additional_agi INTEGER,
+    additional_int INTEGER,
+    additional_wis INTEGER,
+    character_level INTEGER,
+    character_exp DOUBLE,
+    account_stat_id varchar(128) not null,
+    character_id varchar(128) not null,
+    CONSTRAINT fk_account_stat FOREIGN KEY (account_stat_id)
+    REFERENCES account_stats(account_stat_id),
+    CONSTRAINT fk_character FOREIGN KEY (character_id)
+    REFERENCES character_table(character_id)
+    
+);
+
+select "created tables :";
+.tables
+
+.print "todo, add the data insertion here !!! "
+
+.output c:/data.sql
+.dump account_table account_stats character_table account_own_character
+.print "end ! o_O"
