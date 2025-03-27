f7b881fb9be1ac4b7611318a23e7b426657c1a52
Feint <otaku2986@gmail.com>
2025-03-27 18:53:09 +0700

    tData ltf

--- Whiteboard/src/testBoard.cpp
+++ Whiteboard/src/testBoard.cpp
@@ -1,41 +1,82 @@
-
 #include <iostream>
-#include <cmath>
-// Base case: when no digits are left
+#include <string_view>
 
-template <int n, int step>
-int getDecFromBy() {
-    // std::cout << n << "\n";
-    // std::cout << step << "\n";
-    
-    return n;
-    // std::cout << std::endl;
+int isPalindrome(const std::string & s) {
+
+
+    std::string_view sv(s);
+
+    int middle = s.size() / 2;
+    // for(int i = middle; i >0 ; i--) 
+    // {
+        auto left = s.begin();
+        auto right = s.end() - 1;
+        while(left < right) 
+        {
+            if(*left == *right) 
+            {
+                left++;
+                right--;
+            } else 
+            {
+                return false;
+            }
+        }
+        
+        if(left >= right)
+        {
+            return true;
+        }
+
+        return false;
+
 }
 
-// Variadic template to print binary representation
-template <int n, int step, bool first,bool... rest>
-int getDecFromBy() {
-    
+std::string longestPalindrome(const std::string & s) 
+{
+    int middle = s.size() / 2;
+    bool hasMiddle = false;
+    if(s.size() % 2 != 0)
+    {
+        middle++;
+        hasMiddle = true;
+        std::cout << "has middle \n";
+    }
+    for(int i = middle; i > 1 ; i--) 
+    {
+        int diff = middle - (i - hasMiddle);
+        int left = middle - diff;
+        int right = middle + diff;
+        std::string temp = std::string(s.begin() + left, s.begin() + right);
+        std::cout << "temp is " << temp << "\n";
+        // if(isPalindrome(temp))
+        // {
+        //     return temp;
+        // }
+    }
+
+    for(int i = middle; i < s.size() -1 ; i++) 
+    {
+        int diff = middle - i;
+        int left = middle - diff;
+        int right = middle + diff -1;
+        std::string temp = std::string(s.begin() + left, s.begin() + right);
+        std::cout << "temp is " << temp << "\n";
+        // if(isPalindrome(temp))
+        // {
+        //     return temp;
+        // }
+    }
+
  
-    if(first)
-    {
-        std::cout << "got step " << step << "\n"; ;
-        std::cout << "n now is " << step << "\n";
-        return getDecFromBy< step + n ,step * 2 ,rest...>();
-    }
-    else
-    {
-        std::cout << "no \n";
-        std::cout << "n now is " << n << "\n";
-        return getDecFromBy< n ,step * 2 ,rest...>();
-    }
 
-     // Recursive call to process the rest
+    return "";
 }
+
 
 int wmain() {
-    std::cout <<  getDecFromBy<0,1, 1, 0, 1, 1>(); 
-    // std::cout << t << "\n";
+    std::cout << longestPalindrome("babad") << '\n';  
+    std::cout << longestPalindrome("cbbd") << '\n';   
+    std::cout << longestPalindrome("racecar") << '\n'; 
     return 0;
 }
-
