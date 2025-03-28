66840a5772680891ef92e17d9aed70b1901ea99e
Feint <feint@nomail.com>
2025-03-28 21:09:42 +0700

    ltg, expand

--- Whiteboard/src/testBoard.cpp
+++ Whiteboard/src/testBoard.cpp
@@ -1,82 +1,46 @@
 #include <iostream>
 #include <string_view>
 
-int isPalindrome(const std::string & s) {
-
-
-    std::string_view sv(s);
-
-    int middle = s.size() / 2;
-    // for(int i = middle; i >0 ; i--) 
-    // {
-        auto left = s.begin();
-        auto right = s.end() - 1;
-        while(left < right) 
-        {
-            if(*left == *right) 
-            {
-                left++;
-                right--;
-            } else 
-            {
-                return false;
-            }
-        }
-        
-        if(left >= right)
-        {
-            return true;
-        }
-
-        return false;
-
+std::string_view expandFromCenter(std::string_view s, int left, int right) {
+    while (left >= 0 && right < s.size() && s[left] == s[right]) 
+    {
+        left--;
+        right++;
+    }
+    return s.substr(left + 1, right - left - 1);  
 }
 
-std::string longestPalindrome(const std::string & s) 
-{
-    int middle = s.size() / 2;
-    bool hasMiddle = false;
-    if(s.size() % 2 != 0)
-    {
-        middle++;
-        hasMiddle = true;
-        std::cout << "has middle \n";
-    }
-    for(int i = middle; i > 1 ; i--) 
-    {
-        int diff = middle - (i - hasMiddle);
-        int left = middle - diff;
-        int right = middle + diff;
-        std::string temp = std::string(s.begin() + left, s.begin() + right);
-        std::cout << "temp is " << temp << "\n";
-        // if(isPalindrome(temp))
-        // {
-        //     return temp;
-        // }
-    }
+std::string longestPalindrome(std::string_view s) {
+    if (s.empty()) return "";
 
-    for(int i = middle; i < s.size() -1 ; i++) 
+    std::string_view longest;
+    
+    for (int i = 0; i < s.size(); ++i) 
     {
-        int diff = middle - i;
-        int left = middle - diff;
-        int right = middle + diff -1;
-        std::string temp = std::string(s.begin() + left, s.begin() + right);
-        std::cout << "temp is " << temp << "\n";
-        // if(isPalindrome(temp))
-        // {
-        //     return temp;
-        // }
+        std::string_view odd  = expandFromCenter(s, i, i);     
+        std::string_view even = expandFromCenter(s, i, i + 1);  
+
+        if (odd.size() > longest.size())
+        {
+            longest = odd;
+        } 
+        if (even.size() > longest.size())
+        {
+            longest = even;
+        } 
     }
-
- 
-
-    return "";
+    
+    return std::string(longest);
 }
+
 
 
 int wmain() {
     std::cout << longestPalindrome("babad") << '\n';  
     std::cout << longestPalindrome("cbbd") << '\n';   
     std::cout << longestPalindrome("racecar") << '\n'; 
+    std::cout << longestPalindrome("asdasdracecar") << '\n'; 
+    std::cout << longestPalindrome("racecarasdasd") << '\n'; 
+
     return 0;
 }
