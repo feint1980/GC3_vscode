3dbeb9100eab95325c84ff862ca9520ee2f25534
Feint <otaku2986@gmail.com>
2026-05-19 19:19:36 +0700

    tkae

---
+++ Touhou_TB/src/CombatGUIDock_Left.cpp
@@ -0,0 +1,25 @@
+#include "CombatGUIDock_Left.h"
+
+
+CombatGUIDock_Left::CombatGUIDock_Left()
+{
+
+}
+CombatGUIDock_Left::~CombatGUIDock_Left()
+{
+
+}
+
+
+void CombatGUIDock_Left::init(const std::string & filePath)
+{
+
+}
+void CombatGUIDock_Left::draw(Feintgine::SpriteBatch & spriteBatch)
+{
+
+}
+void CombatGUIDock_Left::update(float deltaTime)
+{
+
+}
